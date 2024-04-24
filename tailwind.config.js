92e5fe539d38807cb1ae8341b05363680a165bbe
Matthew Kwon <hyungwoo_128@outlook.com>
2024-04-24 22:55:45 +0900

    [config] Add primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,8 @@ module.exports = {
     },
     extend: {
       colors: {
+        primary_green: "#495E57",
+        primary_yellow: "#F4CE14",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
