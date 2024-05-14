47a600641f23f3725107540f5422bd8ea01403be
Matthew Kwon <hyungwoo_128@outlook.com>
2024-05-14 23:40:53 +0900

    [fix] Fix component to move button to the bottom of the compoenent

--- components/menu-card.tsx
+++ components/menu-card.tsx
@@ -5,19 +5,21 @@ import { buttonVariants } from "@/components/ui/button"
 export function MenuCard({ title, description, price, image }) {
   return (
     <div className="relative overflow-hidden rounded-3xl border bg-gray-200">
-        <div className="flex flex-col rounded-2xl p-2">
+      <div className="flex flex-col h-full rounded-2xl p-2">
         <img src={image} className="h-40 fill-current rounded-2xl"/>
-        <div className="p-3 justify-between">
+        <div className="flex flex-col p-3 justify-between flex-grow">
+          <div>
             <div className="flex justify-between items-center py-2">
-            <h3 className="font-bold">{title}</h3>
-            <p className="font-bold text-xs">{price}</p>
+              <h3 className="font-bold">{title}</h3>
+              <p className="font-bold text-xs">{price}</p>
             </div>
             <p className="text-sm text-muted-foreground pb-4">
-            {description}
+              {description}
             </p>
-            <Link href="/menu" className={cn(buttonVariants({ variant: "default", size: "sm" }))}> View Menu </Link>
+          </div>
+          <Link href="/menu" className={cn(buttonVariants({ variant: "default", size: "sm" }))}> View Menu </Link>
         </div>
-        </div>
+      </div>
     </div>
   )
 }
