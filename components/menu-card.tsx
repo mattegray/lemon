2c88f7699e2fe7e72992f0bbaa147df33a6a7313
Matthew Kwon <hyungwoo_128@outlook.com>
2024-05-14 22:56:35 +0900

    [feat] Add MenuCard component

---
+++ components/menu-card.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link"
+import { cn } from "@/lib/utils"
+import { buttonVariants } from "@/components/ui/button"
+
+export function MenuCard({ title, description, price, image }) {
+  return (
+    <div className="relative overflow-hidden rounded-3xl border bg-gray-200">
+        <div className="flex flex-col rounded-2xl p-2">
+        <img src={image} className="h-40 fill-current rounded-2xl"/>
+        <div className="p-3 justify-between">
+            <div className="flex justify-between items-center py-2">
+            <h3 className="font-bold">{title}</h3>
+            <p className="font-bold text-xs">{price}</p>
+            </div>
+            <p className="text-sm text-muted-foreground pb-4">
+            {description}
+            </p>
+            <Link href="/menu" className={cn(buttonVariants({ variant: "default", size: "sm" }))}> View Menu </Link>
+        </div>
+        </div>
+    </div>
+  )
+}
